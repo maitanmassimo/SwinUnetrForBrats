2f771e8ac86792189eb34082bd87d64158724736
Massimo <massimo.maitan@mail.polimi.it>
2023-11-14 00:57:57 +0100

    added script for test

---
+++ SwinUNETR/BRATS21/custom_evaluate_test.py
@@ -0,0 +1,177 @@
+# Copyright 2020 - 2022 MONAI Consortium
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#     http://www.apache.org/licenses/LICENSE-2.0
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+import time
+import argparse
+import os
+from functools import partial
+
+import numpy as np
+import torch
+import torch.distributed as dist
+import torch.multiprocessing as mp
+import torch.nn.parallel
+import torch.utils.data.distributed
+from optimizers.lr_scheduler import LinearWarmupCosineAnnealingLR
+from trainer import run_training
+from utils.data_utils import get_loader
+from torch.cuda.amp import GradScaler, autocast
+from utils.utils import AverageMeter, distributed_all_gather
+from monai.inferers import sliding_window_inference
+from monai.losses import DiceLoss
+from monai.metrics import DiceMetric
+from monai.networks.nets import SwinUNETR
+from monai.transforms import Activations, AsDiscrete, Compose
+from monai.utils.enums import MetricReduction
+from monai.data import decollate_batch
+
+parser = argparse.ArgumentParser(description="Swin UNETR segmentation pipeline for BRATS Challenge")
+parser.add_argument("--data_dir", default="/dataset/brats2021/", type=str, help="dataset directory")
+parser.add_argument("--logdir", default="test", type=str, help="directory to save the tensorboard logs")
+parser.add_argument("--json_list", default="./jsons/brats21_folds.json", type=str, help="dataset json file")
+parser.add_argument("--fold", default=0, type=int, help="data fold")
+parser.add_argument("--pretrained_model_name", default="model.pt", type=str, help="pretrained model name")
+parser.add_argument("--feature_size", default=48, type=int, help="feature size")
+parser.add_argument("--infer_overlap", default=0.5, type=float, help="sliding window inference overlap")
+parser.add_argument("--in_channels", default=4, type=int, help="number of input channels")
+parser.add_argument("--out_channels", default=3, type=int, help="number of output channels")
+parser.add_argument("--a_min", default=-175.0, type=float, help="a_min in ScaleIntensityRanged")
+parser.add_argument("--a_max", default=250.0, type=float, help="a_max in ScaleIntensityRanged")
+parser.add_argument("--b_min", default=0.0, type=float, help="b_min in ScaleIntensityRanged")
+parser.add_argument("--b_max", default=1.0, type=float, help="b_max in ScaleIntensityRanged")
+parser.add_argument("--space_x", default=1.5, type=float, help="spacing in x direction")
+parser.add_argument("--space_y", default=1.5, type=float, help="spacing in y direction")
+parser.add_argument("--space_z", default=2.0, type=float, help="spacing in z direction")
+parser.add_argument("--roi_x", default=96, type=int, help="roi size in x direction")
+parser.add_argument("--roi_y", default=96, type=int, help="roi size in y direction")
+parser.add_argument("--roi_z", default=96, type=int, help="roi size in z direction")
+parser.add_argument("--rank", default=0, type=int, help="node rank for distributed training")
+parser.add_argument("--dropout_rate", default=0.0, type=float, help="dropout rate")
+parser.add_argument("--distributed", action="store_true", help="start distributed training")
+parser.add_argument("--workers", default=8, type=int, help="number of workers")
+parser.add_argument("--RandFlipd_prob", default=0.2, type=float, help="RandFlipd aug probability")
+parser.add_argument("--RandRotate90d_prob", default=0.2, type=float, help="RandRotate90d aug probability")
+parser.add_argument("--RandScaleIntensityd_prob", default=0.1, type=float, help="RandScaleIntensityd aug probability")
+parser.add_argument("--RandShiftIntensityd_prob", default=0.1, type=float, help="RandShiftIntensityd aug probability")
+parser.add_argument("--spatial_dims", default=3, type=int, help="spatial dimension of input data")
+parser.add_argument("--noamp", action="store_true", help="do NOT use amp for training")
+parser.add_argument("--use_checkpoint", action="store_true", help="use gradient checkpointing to save memory")
+parser.add_argument(
+    "--pretrained_dir",
+    default="./pretrained_models/fold1_f48_ep300_4gpu_dice0_9059/",
+    type=str,
+    help="pretrained checkpoint directory",
+)
+parser.add_argument("--squared_dice", action="store_true", help="use squared Dice")
+
+
+def main():
+    args = parser.parse_args()
+    args.test_mode = True
+    args.amp = not args.noamp
+    test_loader = get_loader(args)
+    pretrained_dir = args.pretrained_dir
+    model_name = args.pretrained_model_name
+    args.gpu=0
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    pretrained_pth = os.path.join(pretrained_dir, model_name)
+    model = SwinUNETR(
+        img_size=(args.roi_x, args.roi_y, args.roi_z),
+        in_channels=args.in_channels,
+        out_channels=args.out_channels,
+        feature_size=args.feature_size,
+        drop_rate=0.0,
+        attn_drop_rate=0.0,
+        dropout_path_rate=0.0,
+        use_checkpoint=args.use_checkpoint,
+    )
+    model_dict = torch.load(pretrained_pth)["state_dict"]
+    model.load_state_dict(model_dict)
+    model.eval()
+    model.to(device)
+
+    
+    model_inferer_test = partial(
+        sliding_window_inference,
+        roi_size=[args.roi_x, args.roi_y, args.roi_z],
+        sw_batch_size=1,
+        predictor=model,
+        overlap=args.infer_overlap,
+    )
+
+    post_sigmoid = Activations(sigmoid=True)
+    post_pred = AsDiscrete(argmax=False, logit_thresh=0.5)
+    dice_acc = DiceMetric(include_background=True, reduction=MetricReduction.MEAN_BATCH, get_not_nans=True)
+
+    model.cuda(args.gpu)
+    with open(os.path.join(args.logdir, "test.csv"), "a") as f:
+                f.write("patient\tDice_TC\tDice_WT\tDice_ET\tMean_Dice\n")
+    val_acc = val_model(
+                model,
+                loader=test_loader,
+                acc_func=dice_acc,
+                model_inferer=model_inferer_test,
+                args=args,
+                post_sigmoid=post_sigmoid,
+                post_pred=post_pred,
+            )
+    return val_acc
+
+
+def val_model(model, loader, acc_func, args, model_inferer=None, post_sigmoid=None, post_pred=None):
+    model.eval()
+    start_time = time.time()
+    run_acc = AverageMeter()
+
+    with torch.no_grad():
+        for idx, batch_data in enumerate(loader):
+            data, target = batch_data["image"], batch_data["label"]
+            data, target = data.cuda(args.rank), target.cuda(args.rank)
+            with autocast(enabled=args.amp):
+                logits = model_inferer(data)
+            val_labels_list = decollate_batch(target)
+            val_outputs_list = decollate_batch(logits)
+            val_output_convert = [post_pred(post_sigmoid(val_pred_tensor)) for val_pred_tensor in val_outputs_list]
+            acc_func.reset()
+            acc_func(y_pred=val_output_convert, y=val_labels_list)
+            acc, not_nans = acc_func.aggregate()
+            mean = torch.mean(acc)
+            acc = acc.cuda(args.rank)
+            
+            #with open(os.path.join(args.logdir, "validation.csv"), "a") as f:
+            #    f.write("epoch\patient n\tDice_TC\tDice_WT\tDice_ET\n")
+
+            with open(
+                os.path.join(args.logdir, "test.csv"), "a"
+            ) as f:
+                f.write(
+                    "{:d}\t{:.5f}\t{:.5f}\t{:.5f}\t{:.5f}\n".format(
+                        idx,
+                        acc[0],
+                        acc[1],
+                        acc[2],
+                        mean
+                    )
+                )
+            if args.distributed:
+                acc_list, not_nans_list = distributed_all_gather(
+                    [acc, not_nans], out_numpy=True, is_valid=idx < loader.sampler.valid_length
+                )
+                for al, nl in zip(acc_list, not_nans_list):
+                    run_acc.update(al, n=nl)
+            else:
+                run_acc.update(acc.cpu().numpy(), n=not_nans.cpu().numpy())
+
+
+
+    return run_acc.avg
+
+if __name__ == "__main__":
+    main()
